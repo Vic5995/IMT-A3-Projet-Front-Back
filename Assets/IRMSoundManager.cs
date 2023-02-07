a1ae89afc3bebc11b77cf0fd09437b21e70e90a6
r20barou <romain.barou@imt-atlantique.net>
2023-02-07 10:52:29 +0100

    ajout sons IRM

--- Assets/IRMSoundManager.cs
+++ Assets/IRMSoundManager.cs
@@ -9,9 +9,15 @@ public class IRMSoundManager : MonoBehaviour
     private bool playerSitDown = false;
 
     [SerializeField] private AudioSource irmSoundEffect;
+    [SerializeField] private AudioSource compresseurSoundEffect;
 
     void Start()
     {
+
+        
+        compresseurSoundEffect.Stop();
+
+
         if (patient == null)
         {
             patient = GameObject.FindGameObjectWithTag("MainCamera");
@@ -20,17 +26,26 @@ public class IRMSoundManager : MonoBehaviour
 
     
     void Update()
-    {
+    {   
         float distance = Vector3.Distance(patient.transform.position, transform.position);
-
-        if(distance <= 4 && !irmSoundEffect.isPlaying)
+        
+        if(distance <= 40 && !irmSoundEffect.isPlaying)
         {
             irmSoundEffect.Play();
         }
 
-        if(distance > 4 && irmSoundEffect.isPlaying)
+        if(distance > 40 && irmSoundEffect.isPlaying)
         {
             irmSoundEffect.Pause();
+        }
+        if (distance <= 40 && !compresseurSoundEffect.isPlaying)
+        {
+            compresseurSoundEffect.Play();
+        }
+
+        if (distance > 40 && compresseurSoundEffect.isPlaying)
+        {
+            compresseurSoundEffect.Pause();
         }
     }
 }
