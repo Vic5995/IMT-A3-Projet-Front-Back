4336e0e50745b68006a60faaa4fc3a3b4c0a9812
Victoire <victoire@kick-start.ro>
2023-02-21 16:55:10 +0100

    sliding and avatar

--- Assets/Sliding_effect.cs
+++ Assets/Sliding_effect.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,21 +7,31 @@ public class Sliding_effect : MonoBehaviour
 {
 
     public Transform destination;
-    public float moveSpeed = 1f;
+    public float moveSpeed = 20f;
+    private float cpt;
 
     // Start is called before the first frame update
     void Start()
     {
         GameObject IRM_Pipe = GameObject.Find("IRM_pipe");
         destination = IRM_Pipe.transform;
+        // destination.position = new Vector3(destination.position.x - 1, destination.position.y, destination.position.z);
+        transform.position = new Vector3(6f, 0f, 6.69f);
+
+        cpt = Math.Abs(Vector3.Distance(transform.position, destination.position)) + 1.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         var step = Time.deltaTime * moveSpeed;
-        if(Vector3.Distance(transform.position, destination.position) >= 0.01f){
-            transform.position = Vector3.MoveTowards(transform.position, destination.position, step);
+        //if (Vector3.Distance(transform.position, destination.position) >= 0.1f)
+        if (cpt > 0)
+        {
+            //transform.position = Vector3.MoveTowards(transform.position, destination.position, step);
+            transform.Translate(0, 0, -step);
+            cpt -= step;
         }
     }
 }
